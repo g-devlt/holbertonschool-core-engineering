69fc0d0e7993e64da47b5b86761af01814105539
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-08-31 12:44:34 +0200

    Initial commit

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    print(f'Language: {"Python"}')
+    print(f'Version: {3}')
+    print(f'Pi approx: {3.1415926535:.2f}')
+    print(f'Computation valid: {True and not False}')
