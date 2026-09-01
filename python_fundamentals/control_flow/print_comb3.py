fefd9cbf97daab6e5e1280caca61207b065a0ed3
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-01 14:40:06 +0200

    P2T5: Fixed styling

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 for x in range(0, 9):
     for y in range(x, 10):
-        print(('{}{}\n' if x == 8 and y == 9 else '{}{}, ').format(x, y), end="")
+        s = '{}{}\n' if x == 8 and y == 9 else '{}{}, '
+        print(s.format(x, y), end="")
