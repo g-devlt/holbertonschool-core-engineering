e02b644903fe3ade571bf310088b85a691a68ddd
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-01 10:59:42 +0200

    Fixed linter errors

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10000, 10000)
-ld = (number % 10) if (number >= 0) else (number % -10) # Modulo 10 gets the last digit in base 10
+ld = (number % 10) if (number >= 0) else (number % -10)
 
 if (ld > 5):
     print(f'Last digit of {number} is {ld} and is greater than 5')
