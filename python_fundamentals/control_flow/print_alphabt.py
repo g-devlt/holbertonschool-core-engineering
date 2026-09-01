40a5999f6fcd8b7a38a0e37478695a43ac319e07
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-01 11:25:48 +0200

    Fixed line too long

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/env python3
-print('{}'.format("".join([chr(x) for x in range(ord('a'), ord('z')+1) if chr(x) not in "eq"])), end="")
+buf = [chr(x) for x in range(ord('a'), ord('z')+1) if chr(x) not in "eq"]
+print('{}'.format("".join(buf)), end="")
