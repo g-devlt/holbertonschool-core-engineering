1160c96d7b576bab88c0a963c15a5c3cde11d4e2
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-01 14:38:12 +0200

    P2T5: Fixed

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-for x in range(0, 10):
+for x in range(0, 9):
     for y in range(x, 10):
-        print(('{}{}, ' if x == 9 and y == 9 else '{}{}').format(x, y))
+        print(('{}{}\n' if x == 8 and y == 9 else '{}{}, ').format(x, y), end="")
