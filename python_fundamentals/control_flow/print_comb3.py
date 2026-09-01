06e8c6925a0433c1738036844381e827475a6b8a
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-01 14:41:11 +0200

    P2T5: Fixed logic

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 for x in range(0, 9):
-    for y in range(x, 10):
+    for y in range(x+1, 10):
         s = '{}{}\n' if x == 8 and y == 9 else '{}{}, '
         print(s.format(x, y), end="")
