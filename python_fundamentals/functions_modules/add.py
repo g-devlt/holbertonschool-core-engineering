839f412b35b7939d7df8e556bdaf1ca8b7d2df6a
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-03 15:50:06 +0200

    P3T5

---
+++ python_fundamentals/functions_modules/add.py
@@ -0,0 +1,7 @@
+from add_0 import add
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    c = a + b
+    print(f'{a} + {b} = {c}')
---
+++ python_fundamentals/functions_modules/add_0.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""Placeholder file incase the checker doesn't provides it"""
+
+def add(a, b):
+    return a + b
