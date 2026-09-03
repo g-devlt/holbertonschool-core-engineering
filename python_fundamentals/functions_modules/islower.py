2f5ec8b5347a40ee6ff1a51853ccc6476bd78452
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-03 12:37:11 +0200

    P3T1: islower

---
+++ python_fundamentals/functions_modules/islower.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def islower(c): 
+    """Checks wether an object is a lowercase character"""
+    if (isinstance(c, str) and len(c) == 1):
+        return ord('a') <= ord(c) <= ord('z')
+    return False
+
+if __name__ == "__main__":
+    print("'a'  :", islower('a'))
+    print("'f'  :", islower('f'))
+    print("'z'  :", islower('z'))
+    print("'A'  :", islower('A'))
+    print("'Z'  :", islower('Z'))
+    print("'-'  :", islower('-'))
+    print("'3'  :", islower('3'))
+    print("3    :", islower(3))
+    print("None :", islower(None))
