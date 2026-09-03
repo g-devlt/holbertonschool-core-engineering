d5f846099b7147391a4a8b4a1b06aad9bd6c8a55
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-03 12:38:44 +0200

    P3T1: Style fix

--- python_fundamentals/functions_modules/islower.py
+++ python_fundamentals/functions_modules/islower.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-def islower(c): 
+def islower(c):
     """Checks wether an object is a lowercase character"""
     if (isinstance(c, str) and len(c) == 1):
         return ord('a') <= ord(c) <= ord('z')
     return False
+
 
 if __name__ == "__main__":
     print("'a'  :", islower('a'))
