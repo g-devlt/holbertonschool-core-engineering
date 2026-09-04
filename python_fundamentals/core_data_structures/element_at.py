6778bc34387c24a23e39c776ab5139bb54188a15
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-04 12:14:21 +0000

    P4T1: Style fixes

--- python_fundamentals/core_data_structures/element_at.py
+++ python_fundamentals/core_data_structures/element_at.py
@@ -4,7 +4,6 @@ def element_at(my_list, idx):
     return my_list[idx] if idx >= 0 and len(my_list) > idx else None
 
 
-
 if __name__ == "__main__":
     print(element_at([1, 2], 2))
     print(element_at([1, 2], -1))
