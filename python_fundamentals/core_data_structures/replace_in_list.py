33a859f9be27f3b372366c36ebde211facccf5fc
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-04 12:27:41 +0000

    P4T2: Overcomplicated

--- python_fundamentals/core_data_structures/replace_in_list.py
+++ python_fundamentals/core_data_structures/replace_in_list.py
@@ -3,10 +3,12 @@
 def replace_in_list(my_list, idx, element):
     if (idx >= len(my_list) or idx < 0):
         return None
-    return [(y if x != idx else element) for x, y in enumerate(my_list)]
+    my_list[idx] = element
+    return my_list
 
 
 if __name__ == "__main__":
     x = [1, 2, 3, 4, 5]
     print(x)
     print(replace_in_list(x, 1, -2))
+    print(x)
