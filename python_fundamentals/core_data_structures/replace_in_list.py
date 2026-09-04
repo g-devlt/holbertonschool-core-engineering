2abf569794f8f7130f02f05a9b04312fd28969f3
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-04 12:28:32 +0000

    P4T2: Typo

--- python_fundamentals/core_data_structures/replace_in_list.py
+++ python_fundamentals/core_data_structures/replace_in_list.py
@@ -2,7 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
     if (idx >= len(my_list) or idx < 0):
-        return None
+        return my_list
     my_list[idx] = element
     return my_list
 
