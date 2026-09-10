70e2e53e350b32a9b10c456f78f0c6af2022acab
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-10 14:22:45 +0000

    P7T3: Style Fix

--- python_oop/inheritance/2-rectangle.py
+++ python_oop/inheritance/2-rectangle.py
@@ -24,8 +24,13 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def __str__(self):
-        return "[{}] {}/{}".format(type(self).__name__, self.__width, self.__height)
-    
+        return "[{}] {}/{}".format(
+                type(self).__name__,
+                self.__width,
+                self.__height
+            )
+
+
 if __name__ == "__main__":
     rect = Rectangle(10, 10)
     print(rect.area())
