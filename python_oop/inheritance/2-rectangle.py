22296dc6da0dacbf7c408e968c065cd7fdca7223
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-10 14:17:03 +0000

    Oops

---
+++ python_oop/inheritance/2-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""A module that implements the interface BaseGeometry
+into the Rectangle class
+"""
+
+BaseGeometry = __import__("base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A simple class that implements BaseGeometry
+    """
+    __width = 0
+    __height = 0
+
+    def area(self) -> int:
+        """Returns the area of this shape
+        """
+        return self.__width * self.__height
+
+    def __init__(self, width: int, height: int):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        return "[{}] {}/{}".format(type(self).__name__, self.__width, self.__height)
+    
+if __name__ == "__main__":
+    rect = Rectangle(10, 10)
+    print(rect.area())
+    print(rect)
