f3a6d53c2b07f05d305ada24078c989a71b8adaf
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-10 14:27:11 +0000

    P7T4-5

---
+++ python_oop/inheritance/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""A module that implements the interface BaseGeometry
+into the Square class
+"""
+
+BaseGeometry = __import__("base_geometry").BaseGeometry
+
+
+class Square(BaseGeometry):
+    """A simple class that implements BaseGeometry
+    """
+
+    def area(self) -> int:
+        """Returns the area of this shape
+        """
+        return self.__size ** 2
+
+    def __init__(self, size: int):
+        self.integer_validator("size", size)
+        self.__size = size
+
+if __name__ == "__main__":
+    sq = Square(10, 10)
+    print(sq.area())
