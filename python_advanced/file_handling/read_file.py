0c4cc34cfc10bb89c42bc52da96a43674f7a7c25
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-09-22 14:10:06 +0200

    T0

---
+++ python_advanced/file_handling/read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This modules provides a function
+that reads and returns the content of a file
+"""
+
+def read_file(filename=""):
+    """Reads and returns the content of a file
+    """
+    with open(filename) as f:
+        print(f.read())
+
+
+if __name__ == "__main__":
+    print(read_file(__file__))
